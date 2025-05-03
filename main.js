20029bc6dcda0db8a8b13b0cab02a372d909bf3b
Steptoe44 <stupullen77@gmail.com>
2025-05-03 21:07:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Turfcaster loaded.");
+    document.body.innerHTML += "<p style='margin-top:20px;'>Model is initializing...</p>";
+});
